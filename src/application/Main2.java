1d86832f6c51f94988c05acb6dcb42c0d228bc04
jean pereira <jeanpereira15@gmail.com>
2023-07-21 22:30:45 -0300

    department update implementation

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -16,11 +16,21 @@ public class Main2 {
 		Department department = departmentDAO.finById(4);
 		System.out.println(department);
 		
-		System.out.println("\n=== TEST 3: department findAll ===");
+		System.out.println("\n=== TEST 2: department findAll ===");
 		List<Department> list = departmentDAO.findAll();
 		for (Department department2 : list) {
 			System.out.println(department2);			
 		}
+		System.out.println("\n=== TEST 3: department insert ===");
+		Department newDepartment = new Department(null, "Music");
+		departmentDAO.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+		
+		System.out.println("\n=== TEST 5: department update ===");
+		department = departmentDAO.finById(6);
+		department.setName("food");
+		departmentDAO.update(department);
+		System.out.println("Update completed");
 	}
 
 }
